43cb163d32e2718e0bbae81c8746791a9dc33698
eempc <evc484@gmail.com>
2020-02-10 10:26:09 +0000

    Add project files.

---
+++ HackerRank/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HackerRank {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+            List<int> a = new List<int> { 3, 3, 3 };
+            List<int> b = new List<int> { 1, 2, 3 };
+
+            List<int> result = CompareTheTriplets(a, b);
+            foreach (var x in result) {
+                Console.WriteLine(x);
+            }
+
+        }
+
+        // https://www.hackerrank.com/challenges/compare-the-triplets/problem
+        public static List<int> CompareTheTriplets(List<int> a, List<int> b) {
+            List<int> result = new List<int> { 0, 0 };
+
+            for (int i = 0; i < a.Count; i++) {
+                if (a[i] == b[i]) continue;
+                if (a[i] > b[i]) result[0]++;
+                else result[1]++;
+            }
+
+            return result;
+        }
+    }
+}
