66e156c11dfd94269f15fc1ecb4bd28a7ef99675
eempc <evc484@gmail.com>
2020-02-10 11:01:59 +0000

    sum array ints

--- HackerRank/Program.cs
+++ HackerRank/Program.cs
@@ -13,6 +13,14 @@ namespace HackerRank {
                 Console.WriteLine(x);
             }
 
+            Console.WriteLine("------");
+
+            Console.WriteLine(Factorial(5));
+
+            Console.WriteLine("------");
+
+            long[] zzz = { 100000010, 100000020, 100000030, 100000040, 100000050 };
+            Console.WriteLine(AVeryBigSum(zzz));
         }
 
         // https://www.hackerrank.com/challenges/compare-the-triplets/problem
@@ -26,6 +34,39 @@ namespace HackerRank {
             }
 
             return result;
+        }
+
+        // Add the ints in an array bearing in mind the pattern of the array
+        // https://www.hackerrank.com/challenges/a-very-big-sum/problem?h_r=next-challenge&h_v=zen
+
+        public static long AVeryBigSum(long[] ar) {
+            // Brute Force Solution
+            long result = 0;
+            foreach (long x in ar) {
+                result += x;
+            }
+            return result;
+
+            // A more sophisticated?
+
+            //int n = ar.Length;
+            //long difference = ar[1] - ar[0];
+            //long result = n * ar[0] + TriangleNumber(n - 1) * difference;
+            //return result;
+        }
+
+        public static int Factorial(int x) {
+            if (x <= 1) return 1;
+            else return x * Factorial(x-1);
+        }
+
+        public static int TriangleNumberRecursive(int x) {
+            if (x <= 1) return 1;
+            else return x + TriangleNumberRecursive(x - 1);
+        }
+
+        public static int TriangleNumberFormula(int x) {
+            return x * (x + 1) / 2;
         }
     }
 }
