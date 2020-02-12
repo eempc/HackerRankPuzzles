de2b645dd3100be5219c71c2999c4af839cf969d
eempc <evc484@gmail.com>
2020-02-12 13:08:57 +0000

    sock puzzle

--- HackerRank/Program.cs
+++ HackerRank/Program.cs
@@ -13,7 +13,7 @@ namespace HackerRank {
                 Console.WriteLine(x);
             }
 
-            Console.WriteLine("------");
+            Console.WriteLine("------ Factorial 5 test -------------");
 
             Console.WriteLine(Factorial(5));
 
@@ -21,6 +21,12 @@ namespace HackerRank {
 
             long[] zzz = { 100000010, 100000020, 100000030, 100000040, 100000050 };
             Console.WriteLine(AVeryBigSum(zzz));
+
+            Console.WriteLine("-------- Sock Merchant ----------------");
+
+            string[] sockArray = { "blue", "blue", "blue", "red", "red", "yellow", "yellow", "yellow", "yellow" };
+
+            Console.WriteLine(OddSocks(sockArray));
         }
 
         // https://www.hackerrank.com/challenges/compare-the-triplets/problem
@@ -55,6 +61,30 @@ namespace HackerRank {
             //return result;
         }
 
+        // https://www.hackerrank.com/challenges/sock-merchant/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=warmup
+        public static int OddSocks(string[] ar) {
+            Dictionary<string, int> inventory = new Dictionary<string, int>();
+            int totalPairs = 0;
+
+            foreach (string x in ar) {
+                if (inventory.ContainsKey(x)) inventory[x]++; // ContainsKey is an O(1) operation
+                else inventory.Add(x, 1);
+
+                if (inventory[x] % 2 == 0) totalPairs++; // Instead of the additional iteration below, do the check for pairs here
+            }
+         
+            // Using this additional iteration was O(n) + O(m)
+            //foreach (KeyValuePair<int, int> entry in inventory) {
+            //    total += entry.Value / 2;
+            //}
+
+            return totalPairs;
+        }
+
+
+
+        // Helper methods
+
         public static int Factorial(int x) {
             if (x <= 1) return 1;
             else return x * Factorial(x-1);
@@ -68,5 +98,7 @@ namespace HackerRank {
         public static int TriangleNumberFormula(int x) {
             return x * (x + 1) / 2;
         }
+
+
     }
 }
