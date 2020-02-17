6218f1bdaade8154bcde5ff104d9f15793058c07
eempc <evc484@gmail.com>
2020-02-17 19:09:24 +0000

    magazine

--- HackerRank/Exercises/DictionariesHashMaps.cs
+++ HackerRank/Exercises/DictionariesHashMaps.cs
@@ -4,5 +4,33 @@ using System.Text;
 
 namespace HackerRank.Exercises {
     class DictionariesHashMaps {
+        public static void checkMagazine(string[] magazine, string[] note) {
+            string possible = "Yes";
+
+            Dictionary<string, int> wordFrequency = new Dictionary<string, int>();
+
+            foreach (string word in magazine) {
+                if (wordFrequency.ContainsKey(word)) {
+                    wordFrequency[word]++;
+                } else {
+                    wordFrequency.Add(word, 1);
+                }
+            }
+
+            foreach (string word in note) {
+                if (wordFrequency.ContainsKey(word)) {
+                    wordFrequency[word]--;
+                    if (wordFrequency[word] < 0) {
+                        possible = "No";
+                        break;
+                    }
+                } else {
+                    possible = "No";
+                    break;
+                }
+            }
+
+            Console.WriteLine(possible);
+        }
     }
 }
