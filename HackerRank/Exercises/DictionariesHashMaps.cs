fa08faf04780abb3ec517b04791d5c4ac63f0e0c
eempc <evc484@gmail.com>
2020-02-19 09:51:13 +0000

    TwoString

--- HackerRank/Exercises/DictionariesHashMaps.cs
+++ HackerRank/Exercises/DictionariesHashMaps.cs
@@ -4,6 +4,44 @@ using System.Text;
 
 namespace HackerRank.Exercises {
     class DictionariesHashMaps {
+
+        public static string TwoStrings(string s1, string s2) {
+            // The trick is in the sentence, "one character"
+            string possible = "NO";
+
+            foreach (char c in s1) {
+                if (s2.Contains(c.ToString())) {
+                    possible = "YES";
+                    break;
+                }
+            }
+
+            return possible;
+        }
+
+        public static string TwoStrings2(string s1, string s2) {
+            string possible = "NO";
+
+            Dictionary<string, int> dict = new Dictionary<string, int>();
+
+            foreach (char c in s1) {
+                if (!dict.ContainsKey(c.ToString())) {
+                    dict.Add(c.ToString(), 1);
+                }
+            }
+
+            foreach (char c in s2) {
+                if (dict.ContainsKey(c.ToString())) {
+                    possible = "YES";
+                    break;
+                }
+            }
+
+            return possible;
+        }
+
+
+
         public static void checkMagazine(string[] magazine, string[] note) {
             string possible = "Yes";
 
