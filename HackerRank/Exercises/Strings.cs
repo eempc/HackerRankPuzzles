6b0a7a62bfab02e3973390b822bfa07a2849fa28
eempc <evc484@gmail.com>
2020-02-26 09:29:44 +0000

    MakeAnagrams

---
+++ HackerRank/Exercises/Strings.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HackerRank.Exercises {
+    public class Strings {
+
+        public static int MakeAnagrams(string a, string b) {
+            int count = 0;
+
+            Dictionary<char, int> dict = new Dictionary<char, int>();
+
+            foreach (char c in a) {
+                if (dict.ContainsKey(c)) {
+                    dict[c]++;
+                } else {
+                    dict.Add(c, 1);
+                }
+            }
+
+            foreach (char c in b) {
+                if (dict.ContainsKey(c)) {
+                    dict[c]--;
+                } else {
+                    dict.Add(c, -1);
+                }
+            }
+
+            foreach (KeyValuePair<char, int> kvp in dict) {
+                if (kvp.Value < 0) {
+                    count -= kvp.Value;
+                } else {
+                    count += kvp.Value;
+                }
+            }
+
+            return count;
+        }
+
+    }
+}
