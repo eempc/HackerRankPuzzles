fbbeaf5472681dd589ad5855922ee77f49356edf
eempc <evc484@gmail.com>
2020-03-17 14:54:12 +0000

    Fib

---
+++ HackerRank/Exercises/Recursion.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HackerRank.Exercises {
+    class Recursion {
+        static int Fibonacci(int n) {
+            if (n <= 1) return n;
+            if (n == 2) return 1;
+            return Fibonacci(n - 1) + Fibonacci(n - 2);
+        }
+
+
+    }
+}
