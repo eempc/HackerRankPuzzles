8309e38d04de782e40b7d6fffb9f20b8273dcebf
eempc <evc484@gmail.com>
2020-03-17 15:40:49 +0000

    didnt work superdigit

--- HackerRank/Exercises/Recursion.cs
+++ HackerRank/Exercises/Recursion.cs
@@ -10,6 +10,47 @@ namespace HackerRank.Exercises {
             return Fibonacci(n - 1) + Fibonacci(n - 2);
         }
 
+        static int SuperDigit(string n, int k) {
+            if (n.Length == 1) {
+                return n[0] - '0';
+            }
+
+            int sum = 0;
+            for (int i = 0; i < n.Length; i++) {
+                sum += n[i] - '0';
+            }
+            sum *= k;
+
+            return SuperDigit(sum.ToString(), 1);
+
+        }
+
+        static int DigitSum(string n, int k) {
+            int result = 0;
+            foreach (char c in n) {
+                result += c - '0';
+            }
+
+            result *= k;
+
+            return DigitSumHelper(result);
+        }
+
+        static int DigitSumHelper(int x) {
+            if (x < 10) {
+                return x;
+            }
+
+            return DigitSumHelper(DigitSumAdder(x));
+        }
+
+        static int DigitSumAdder(int x) {
+            if (x == 0) {
+                return 0;
+            }
+
+            return x % 10 + DigitSumAdder(x / 10);
+        }
 
     }
 }
