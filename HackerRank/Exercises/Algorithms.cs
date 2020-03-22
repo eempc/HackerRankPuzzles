08a3a3e270092e11b2bcabe1a226e34c2a33f3e9
eempc <evc484@gmail.com>
2020-03-22 15:37:30 +0000

    apples oranges

--- HackerRank/Exercises/Algorithms.cs
+++ HackerRank/Exercises/Algorithms.cs
@@ -5,16 +5,35 @@ using System.Text;
 namespace HackerRank.Exercises {
     class Algorithms {
 
+        static void countApplesAndOranges(int s, int t, int a, int b, int[] apples, int[] oranges) {
+            int countApples = 0;
+            int countOranges = 0;
+
+            for (int i = 0; i < apples.Length; i++) {
+                if (apples[i] + a >= s && apples[i] + a <= t) {
+                    countApples++;
+                }
+            }
+
+            foreach (int x in oranges) {
+                if (x + b <= t && x + b >= s) {
+                    countOranges++;
+                }
+            }
+
+            Console.WriteLine(countApples);
+            Console.WriteLine(countOranges);
+        }
+
+
         static List<int> GradingStudents(List<int> grades) {
             List<int> result = new List<int>();
 
             foreach (int x in grades) {
                 if (x < 38) {
                     result.Add(x);
-                } else if (x % 10 == 3 || x % 10 == 4) {
-                    result.Add(x + 5 - (x % 10));
-                } else if (x % 10 == 8 || x % 10 == 9) {
-                    result.Add(x + 10 - (x % 10));
+                } else if (x % 5 > 2) {
+                    result.Add(x + 5 - (x % 5));
                 } else {
                     result.Add(x);
                 }
