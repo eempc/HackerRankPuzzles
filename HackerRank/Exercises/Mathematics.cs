c4cf2a72c6546125f7498072bdfd41c9b3e23f96
eempc <evc484@gmail.com>
2020-04-05 12:14:30 +0100

    Max Draws socks

--- HackerRank/Exercises/Mathematics.cs
+++ HackerRank/Exercises/Mathematics.cs
@@ -4,11 +4,19 @@ using System.Text;
 
 namespace HackerRank.Exercises {
     class Mathematics {
+
+        static int maximumDraws(int n) {
+            /*
+             * Write your code here.
+             */
+            return n + 1;
+        }
+
         static int[] findPoint(int px, int py, int qx, int qy) {
-            // Given two coordinates (x1, y2), (x2, y2), find the point reflection aka inversion
-            // Basically it is opposite of finding the midpoint
-            // But technically there are two refletion points when given two coordinates
-            // But we are tasked with the finding one based on the order of the given points
+            // Three points in a straight line:
+            // 1: Start = 0, 0
+            // 2: Mid = 3, 3
+            // 3: Reflection = 6, 6
             int[] result = new int[2];
 
             result[0] = qx - px + qx; // Simplified to (2 * qx - px)
