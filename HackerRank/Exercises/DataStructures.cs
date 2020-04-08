bfaca6ccb9bfed758b79b2627c11b1741c4b465b
eempc <evc484@gmail.com>
2020-04-08 12:17:46 +0100

    manual single linked list

---
+++ HackerRank/Exercises/DataStructures.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HackerRank.Exercises {
+    public static class DataStructures {
+
+             
+     public class SinglyLinkedListNode {
+          public int data;
+          public SinglyLinkedListNode next;
+     }
+     
+     
+    static void printLinkedList(SinglyLinkedListNode head) {
+            Console.WriteLine(head.data);
+            if (head.next != null) {
+                printLinkedList(head.next);
+            }6 
+    }
+
+
+}
+}
