f6861b395b4bccdaf4f61306a9ec707bfef067eb
eempc <evc484@gmail.com>
2020-04-09 13:13:52 +0100

    InsertNodeSinglyList?

--- HackerRank/Exercises/DataStructures.cs
+++ HackerRank/Exercises/DataStructures.cs
@@ -5,21 +5,42 @@ using System.Text;
 namespace HackerRank.Exercises {
     public static class DataStructures {
 
-             
-     public class SinglyLinkedListNode {
-          public int data;
-          public SinglyLinkedListNode next;
-     }
-     
-     
-    static void printLinkedList(SinglyLinkedListNode head) {
+        static SinglyLinkedListNode insertNodeAtTail(SinglyLinkedListNode head, int data) {
+            SinglyLinkedListNode newNode = new SinglyLinkedListNode(data);
+            
+            if (head == null) {
+                head = newNode;
+            } else {
+                SinglyLinkedListNode tempNode = head;
+
+                while (tempNode.next != null) {
+                    tempNode = tempNode.next;
+                }
+
+                tempNode.next = newNode; // undertsanding reference types and how you modify this
+            }
+            return head;
+        }
+
+
+        public class SinglyLinkedListNode {
+            public int data { get; set; }
+            public SinglyLinkedListNode next { get; set; }
+
+            public SinglyLinkedListNode(int datum) {
+                data = datum;
+            }
+        }
+
+
+        static void printLinkedList(SinglyLinkedListNode head) {
             Console.WriteLine(head.data);
             if (head.next != null) {
                 printLinkedList(head.next);
             }
             // If you want the list printed in reverse, put writeline after the if statement
+        }
+
+
     }
-
-
-}
 }
